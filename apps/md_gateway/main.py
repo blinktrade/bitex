de6f2841b8741055f8fd51c3b097a4e1ae69bc64
rodrigo <rodrigo@temporadafora.com>
2013-05-13 09:26:03 -0400

    implemented md_gateway main function

--- apps/md_gateway/main.py
+++ apps/md_gateway/main.py
@@ -15,14 +15,33 @@ import tornado.httpserver
 import tornado.template
 
 from md_gateway import  config
+from views import MdGatewayHandler
 
 class MdGatewayApplication(tornado.web.Application):
   def __init__(self):
     handlers = [
-      (r'/trade', MdGatewayHandler)
-      ]
+      (r'/market_data', MdGatewayHandler)
+    ]
     settings = dict(
       cookie_secret=config.cookie_secret
     )
     tornado.web.Application.__init__(self, handlers, **settings)
 
+    tornado.ioloop.IOLoop.instance().add_timeout(timedelta(seconds=1), self.cron_check_md_updates)
+
+  def cron_check_md_updates(self):
+    # run it again 5 seconds later...
+    tornado.ioloop.IOLoop.instance().add_timeout(timedelta(milliseconds=500), self.cron_check_md_updates)
+
+
+
+
+def main():
+  application = MdGatewayApplication()
+  http_server = tornado.httpserver.HTTPServer(application)
+  http_server.listen(8000)
+
+  tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+  main()
