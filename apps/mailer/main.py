3296aefb55d26fde7c13aaaeb3010f473710317c
root <root@btcsp.(none)>
2013-05-24 13:45:39 +0000

    Sending UTF-8 emails

--- apps/mailer/main.py
+++ apps/mailer/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -13,12 +13,70 @@ from datetime import timedelta
 import  time
 from bitex.client import BitExThreadedClient
 
-import smtplib
+from smtplib import SMTP
+from email.MIMEText import MIMEText
+from email.Header import Header
+from email.Utils import parseaddr, formataddr
+
+
+def send_email(sender, recipient, subject, body):
+    """Send an email.
+
+    All arguments should be Unicode strings (plain ASCII works as well).
+
+    Only the real name part of sender and recipient addresses may contain
+    non-ASCII characters.
+
+    The email will be properly MIME encoded and delivered though SMTP to
+    localhost port 25.  This is easy to change if you want something different.
+
+    The charset of the email will be the first one out of US-ASCII, ISO-8859-1
+    and UTF-8 that can represent all the characters occurring in the email.
+    """
+
+    # Header class is smart enough to try US-ASCII, then the charset we
+    # provide, then fall back to UTF-8.
+    header_charset = 'ISO-8859-1'
+
+    # We must choose the body charset manually
+    for body_charset in 'US-ASCII', 'ISO-8859-1', 'UTF-8':
+        try:
+            body.encode(body_charset)
+        except UnicodeError:
+            pass
+        else:
+            break
+
+    # Split real name (which is optional) and email address parts
+    sender_name, sender_addr = parseaddr(sender)
+    recipient_name, recipient_addr = parseaddr(recipient)
+
+    # We must always pass Unicode strings to Header, otherwise it will
+    # use RFC 2047 encoding even on plain ASCII strings.
+    sender_name = str(Header(unicode(sender_name), header_charset))
+    recipient_name = str(Header(unicode(recipient_name), header_charset))
+
+    # Make sure email addresses do not contain non-ASCII characters
+    sender_addr = sender_addr.encode('ascii')
+    recipient_addr = recipient_addr.encode('ascii')
+
+    # Create the message ('plain' stands for Content-Type: text/plain)
+    msg = MIMEText(body.encode(body_charset), 'plain', body_charset)
+    msg['From'] = formataddr((sender_name, sender_addr))
+    msg['To'] = formataddr((recipient_name, recipient_addr))
+    msg['Subject'] = Header(unicode(subject), header_charset)
+
+    # Send the message via SMTP to localhost:25
+    smtp = SMTP("127.0.0.1")
+    smtp.ehlo()
+    smtp.sendmail(sender, recipient, msg.as_string())
+    smtp.quit()
+
 
 def main():
   while  True:
     try:
-      ws = BitExThreadedClient('wss://localhost:8443/trade')
+      ws = BitExThreadedClient('wss://www.bitex.com.br:8443/trade')
       def on_login(sender, msg):
         ws.sendMsg( {'MsgType':'S0', 'EmailReqID':'0' } )
 
@@ -26,15 +84,10 @@ def main():
       def on_message(sender, msg):
         if msg['MsgType'] == 'C':
           try:
-            body = msg['To']
-            if not len(body):
-              body = '.'
-
-            smtpObj = smtplib.SMTP('127.0.0.1')
-            smtpObj.ehlo()
-            smtpObj.sendmail('bzero@bitex.com.br', [ msg['To'] ], body)
+            sender = u'BitEx Suporte <suporte@bitex.com.br>'
+            send_email (sender, msg['To'], msg['Subject'], msg['Body'] )
           except Exception as ex:
-            print "Error: unable to send email to " + str(msg['To'])
+            print "Error: unable to send email to " + str(msg['To']) + ' - ' + str(ex) 
             
         else:
           print 'received ' , msg
@@ -47,7 +100,7 @@ def main():
       ws.connect()
 
       # TODO: get the user and password from a configuration file
-      ws.login('mailer','abc123')
+      ws.login('mailer','abc123$%')
 
       ws.run_forever()
 
