21c92e56ac31e7cdfaba5dc34ddab6a53f9ccb33
Clebson Derivan <bzero@bitex.com.br>
2013-06-06 13:34:51 +0000

    added server class

--- apps/bitcoiner/main.py
+++ apps/bitcoiner/main.py
@@ -1,13 +1,17 @@
 import pickle
 import json
+import os
+import sys
 
 from jsonrpc import ServiceProxy
 
 from twisted.internet import task, reactor, protocol
 from twisted.protocols import basic
 
-accounts = {}
-factory = None
+ROOT_PATH = os.path.abspath( os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.insert( 0, os.path.join(ROOT_PATH, 'libs'))
+
+from bitex.secret import Secret
 
 class BtcProtocol(basic.LineReceiver):
     def __init__(self, factory):
@@ -22,12 +26,10 @@ class BtcProtocol(basic.LineReceiver):
 
     def lineReceived(self, line):
         print "received", repr(line)
-
-        global accounts
         try:
             msg = json.loads(str(line))
-            if msg['address'] in accounts:
-                self.sendLine(json.dumps(accounts[msg['address']]))
+            if msg['address'] in self.accounts:
+                self.sendLine(json.dumps(self.accounts[msg['address']]))
             else:
                 self.sendLine(json.dumps({"address":msg['address'], "amount":  0, "confirmations": 0}))
 
@@ -46,54 +48,58 @@ class BtcFactory(protocol.Factory):
         for c in self.clients:
             c.sendLine(json.dumps(line))
 
-def load_file():
-    global accounts
-    try:
-        output = open('data.pkl', 'r+')
+class BtcServer:
+
+    def __init__(self):
+        self.accounts = {}
+        self.secret = Secret()
+
+    def start(self):
+        if self.secret.prompt_decrypt() != self.secret.S_FAIL_FATAL:
+            self.connection = ServiceProxy("http://%s:%s@127.0.0.1:8332" % (self.secret.key, self.secret.secret))
+            self.load_file()
+            self.factory = BtcFactory() 
+            task.LoopingCall(self.check_positions).start(0.5)
+            reactor.listenTCP(60025, self.factory)
+            reactor.run()
+
+    def load_file(self):
         try:
-            accounts = pickle.load(output)
-        except EOFError:
-            print 'error reading'
-    except IOError:
-        pass
+            output = open('data.pkl', 'r+')
+            try:
+                self.accounts = pickle.load(output)
+            except EOFError:
+                print 'error reading'
+        except IOError:
+            pass
 
-def save_file():
-    global accounts
-    output = open('data.pkl', 'wb')
-    pickle.dump(accounts, output)
+    def save_file(self):
+        output = open('data.pkl', 'wb')
+        pickle.dump(self.accounts, output)
 
-access = ServiceProxy("http://btcrcp:Xi6xn7N5Rsyya1LYr8ICl351cRpe0xbF2Yub6JZNxRzOfU2zfOmvmNkwfo3dE5LYArzhZ35kdS0V1y57zflaz0lOL@127.0.0.1:8332")
-
-def check_positions():
-    global accounts
-    global factory
-    for acct in access.listreceivedbyaddress(0):
-        address = acct['address']
-        cfmt = acct['confirmations']
-        amt = acct['amount']
-        if address in accounts:
-            prev_amt = accounts[address]['amount']
-            if amt != prev_amt:
-                accounts[address]['amount'] = amt
-                factory.publish( accounts[address])
-                print 'account %s has a new amount %f previous %f' % (address, amt, prev_amt)
-            prev_cfmt = accounts[address]['confirmations']
-            if cfmt != prev_cfmt:
-                accounts[address]['confirmations'] = cfmt
-                save_file()
-                factory.publish( accounts[address])
-                print 'account %s has a new confirmation %d previous %d' % (address, cfmt, prev_cfmt)
-        else:
-            accounts[address] = {'address':address, 'amount':  amt, 'confirmations': cfmt}
-            save_file()
-            print 'account %s position %f confirmations %d' % (address, amt, cfmt)
+    def check_positions(self):
+        for acct in self.connection.listreceivedbyaddress(0):
+            address = acct['address']
+            cfmt = acct['confirmations']
+            amt = acct['amount']
+            if address in self.accounts:
+                prev_amt = self.accounts[address]['amount']
+                if amt != prev_amt:
+                    self.accounts[address]['amount'] = amt
+                    self.factory.publish( self.accounts[address])
+                    print 'account %s has a new amount %f previous %f' % (address, amt, prev_amt)
+                prev_cfmt = self.accounts[address]['confirmations']
+                if cfmt != prev_cfmt:
+                    self.accounts[address]['confirmations'] = cfmt
+                    self.save_file()
+                    self.factory.publish( accounts[address])
+                    print 'account %s has a new confirmation %d previous %d' % (address, cfmt, prev_cfmt)
+            else:
+                self.accounts[address] = {'address':address, 'amount':  amt, 'confirmations': cfmt}
+                self.save_file()
+                print 'account %s position %f confirmations %d' % (address, amt, cfmt)
 
 if __name__ == "__main__":
-    load_file()
-    factory = BtcFactory() 
-    task.LoopingCall(check_positions).start(0.5)
-
-    reactor.listenTCP(60025, factory)
-    reactor.run()
+    BtcServer().start()
 
 
