92541d297076f1b2cb2f25268f46113749df377a
rodrigo <rodrigo@temporadafora.com>
2014-01-13 04:19:36 -0500

    added new decorator functions

--- apps/trade/decorators.py
+++ apps/trade/decorators.py
@@ -2,7 +2,28 @@ from errors import *
 
 def login_required(func):
   def decorator(session,*args, **kwargs):
-    if not session.is_logged:
+    if session.user is None :
+      raise NotAuthorizedError()
+    return func(session, *args, **kwargs)
+  return decorator
+
+def staff_user_required(func):
+  def decorator(session,*args, **kwargs):
+    if session.user is None or session.user.is_staff == False:
+      raise NotAuthorizedError()
+    return func(session, *args, **kwargs)
+  return decorator
+
+def system_user_required(func):
+  def decorator(session,*args, **kwargs):
+    if session.user is None or session.user.is_system == False:
+      raise NotAuthorizedError()
+    return func(session, *args, **kwargs)
+  return decorator
+
+def verified_user_required(func):
+  def decorator(session,*args, **kwargs):
+    if session.user is None or session.user.verified == False:
       raise NotAuthorizedError()
     return func(session, *args, **kwargs)
   return decorator
