5b5ae156466e3fd79ab595d1f749fad81cad46d2
rodrigo <rodrigo@bitex.com>
2014-07-06 18:00:07 -0400

    applied patch https://code.google.com/p/closure-charts/source/browse/closure-library-patches/SvgGraphicsGetTextWidth.patch

--- jsdev/closure-library/closure/goog/graphics/svggraphics.js
+++ jsdev/closure-library/closure/goog/graphics/svggraphics.js
@@ -717,7 +717,37 @@ goog.graphics.SvgGraphics.prototype.createGroup = function(opt_group) {
  * @override
  */
 goog.graphics.SvgGraphics.prototype.getTextWidth = function(text, font) {
-  // TODO(user) Implement
+  /** @type {goog.dom.DomHelper} */
+  var domHelper = goog.dom.getDomHelper();
+
+  /**
+   * The ruler is used to measure the pixel width of Strings The style given
+   * allows the ruler to be off the page (out of sight), with the correct font
+   * and style properties.
+   *
+   * @type {Element}
+   */
+  var ruler_ = domHelper.createDom(
+      'div',
+      {
+        style: 'position:absolute; visibility:hidden; font-family:' +
+            font.family + '; font-size:' +
+            font.size + 'px;'
+      });
+
+  // Add the ruler to the dom
+  document.body.appendChild(ruler_);
+
+  // Set the inner html of the ruler to be the text
+  ruler_.innerHTML = text;
+
+  /** @type {number} */
+  var result = ruler_.offsetWidth;
+
+  // Remove the ruler
+  domHelper.removeNode(ruler_);
+
+  return result;
 };
 
 
