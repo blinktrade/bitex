bdc26dd7f043b90fd5e1c7d10c1367d28ca09e77
Marcel <marcel.mdr@gmail.com>
2014-07-14 15:53:46 -0400

    Include MerchantApp

--- jsdev/build_release.sh
+++ jsdev/build_release.sh
@@ -134,3 +134,32 @@ python ./closure-library/closure/bin/build/closurebuilder.py  \
 #  --compiler_flags="--formatting=PRETTY_PRINT" \
 #  --compiler_flags="--formatting=PRINT_INPUT_DELIMITER" \
 #   > ../static/js/bitex_app_blink_trade.compiled.pt_BR.js
+
+
+
+# BlinkTrade Application - enUS
+python ./closure-library/closure/bin/build/closurebuilder.py  \
+  --root=./closure-library/ \
+  --root=./closure-bootstrap/javascript/ \
+  --root=./scottlogic/ \
+  --root=./bitex \
+  --namespace=bitex.app.MerchantApp \
+  --output_mode=compiled \
+  --compiler_jar=./tools/compiler.jar \
+  --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
+  --compiler_flags="--define=goog.DEBUG=0" \
+  --compiler_flags="--define=goog.LOCALE='en_US'" \
+  --compiler_flags="--externs=./externs/google.js" \
+  --compiler_flags="--externs=./externs/plusone.js" \
+  --compiler_flags="--externs=./externs/jquery-1.9.js" \
+  --compiler_flags="--externs=./externs/twitter-bootstrap.js" \
+  --compiler_flags="--externs=./externs/parsley.js" \
+  --compiler_flags="--externs=./externs/sticky.js" \
+  --compiler_flags="--externs=./externs/jquerymobile-1.4.3.js" \
+  --compiler_flags="--debug=TRUE" \
+  --compiler_flags="--formatting=PRETTY_PRINT" \
+  --compiler_flags="--formatting=PRINT_INPUT_DELIMITER" \
+  --compiler_flags="--externs=./externs/facebook_javascript_sdk.js" \
+  --compiler_flags="--translations_project='bitex'" \
+  --compiler_flags="--translations_file=./translations/en.xtb.xml" \
+   > ../static/js/bitex_app_merchant.compiled.en_US.js
