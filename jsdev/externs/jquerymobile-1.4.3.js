58509663f0dc29f3c00d1f8731ee6a41acbf6a1d
Marcel <marcel.mdr@gmail.com>
2014-07-14 15:55:22 -0400

    Implemantation page Marchant
    Implementation jquerymobile-1.4.3 file

---
+++ jsdev/externs/jquerymobile-1.4.3.js
@@ -0,0 +1,16 @@
+
+
+/**
+ * @constructor
+ * @private
+ */
+jQuery.mobile = function () {};
+
+/**
+ *
+ * @param {string} to
+ * @param {Object=} opt_options
+ */
+jQuery.mobile.prototype.changePage = function(to, opt_options) {};
+
+
