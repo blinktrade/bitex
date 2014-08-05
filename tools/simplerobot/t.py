ec8aab10e04207623872fe3c89e8cde9ebd98f7a
bzero <cderivan@gmail.com>
2014-08-05 15:29:49 +0000

    removed unused file

--- tools/simplerobot/t.py
+++
@@ -1,8 +0,0 @@
-import json
-import urllib2
-
-response = urllib2.urlopen('http://www.bitcointoyou.com/API/orderbook.aspx')
-#data = json.load(urllib2.urlopen('http://www.bitcointoyou.com/API/orderbook.aspx'))
-
-
-print response
