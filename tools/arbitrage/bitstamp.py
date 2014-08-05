889c5732c6f296ceefcbf88b6b42c2b040411ef5
rodrigo <rodrigo@bitex.com>
2014-08-05 05:07:30 -0400

    included new arbitrators

--- tools/arbitrage/bitstamp.py
+++ tools/arbitrage/bitstamp.py
@@ -6,47 +6,63 @@ import logging
 ROOT_PATH = os.path.abspath( os.path.join(os.path.dirname(__file__), "../../"))
 sys.path.insert( 0, os.path.join(ROOT_PATH, 'libs'))
 
-from bitex.client import BitExThreadedClient
 import pusherclient
 import json
-from order_book_processor import OrderBookProcessor
-from util import get_funded_entries
 
 import datetime
 import hmac
 import hashlib
+from arbitrator import BlinkTradeArbitrator
+from time import sleep
 
-class BitstampArbitrator(object):
-  def __init__(self, bitex_username, bitex_password,  bitex_ws_url='wss://api.blinktrade.com/trade/', bid_fee=0, ask_fee=0, api_key=None, api_secret=None):
+
+class BitStampClient(object):
+  def __init__(self, username,password,websocket_url, bid_fee=0., ask_fee=0., api_key=None, api_secret=None):
     self.pusher = pusherclient.Pusher(key= 'de504dc5763aeef9ff52', log_level=logging.ERROR)
-    self.order_book_bid_processor = OrderBookProcessor('1', 'BTCUSD')
-    self.order_book_ask_processor = OrderBookProcessor('2', 'BTCUSD')
-    self.bid_fee = bid_fee
-    self.ask_fee = ask_fee
     self.api_key = api_key
     self.api_secret = api_secret
+    self.bid_fee = bid_fee
+    self.ask_fee = ask_fee
 
-    self.ws = BitExThreadedClient( bitex_ws_url )
-    self.bitex_username = bitex_username
-    self.bitex_password = bitex_password
+    self.arbitrator = BlinkTradeArbitrator(username,password,websocket_url, 'BTCUSD')
+    self.arbitrator.signal_order.connect(self.on_send_order_to_bitstamp)
 
-    self.order_book_bid_processor.send_new_order_signal.connect(self.on_send_buy_new_order)
-    self.order_book_ask_processor.send_new_order_signal.connect(self.on_send_sell_new_order)
-    self.order_book_bid_processor.cancel_order_signal.connect(self.on_send_cancel_order)
-    self.order_book_ask_processor.cancel_order_signal.connect(self.on_send_cancel_order)
+  def connect(self):
+    self.pusher.connection.bind('pusher:connection_established', self.on_bitstamp_connect_handler)
+    self.pusher.connection.bind('pusher:connection_failed', self.on_bitstamp_connect_failed_handler)
 
-    self.usd_balance = 0
-    self.btc_balance = 0
-    self.bitex_broker = None
-    self.bitex_profile = None
+    self.pusher.connect()
+    self.arbitrator.connect_to_blinktrade()
 
-  def on_bitex_execution_report(self, sender, msg):
-    if msg['ExecType'] == '0' or msg['ExecType'] == '4': # cancel
-      return
+  def disconnect(self):
+    self.pusher.disconnect()
+    self.arbitrator.cancel_all_orders()
+    self.arbitrator.close()
+
+  def keep_alive(self):
+    self.arbitrator.send_testRequest()
+
+  def on_bitstamp_connect_failed_handler(self, data):
+    self.arbitrator.cancel_all_orders()
 
 
-    print datetime.datetime.now(), msg
+  def on_bitstamp_connect_handler(self, data):
+    print 'connected to bitstamp'
+    channel = self.pusher.subscribe('order_book')
+    channel.bind('data', self.on_bitstamp_order_book_handler )
 
+  def on_bitstamp_order_book_handler(self, data):
+    print data
+    self.arbitrator.send_testRequest()
+
+    data = json.loads(data)
+    bid_list = [  [  int(float(usd)*1e8 * (1. - self.bid_fee) ), int(float(btc) * 1e8) ]  for usd,btc in data['bids'] ]
+    ask_list = [  [  int(float(usd)*1e8 * (1. + self.ask_fee) ), int(float(btc) * 1e8) ]  for usd,btc in data['asks'] ]
+    self.arbitrator.process_ask_list(ask_list)
+    self.arbitrator.process_bid_list(bid_list)
+
+
+  def on_send_order_to_bitstamp(self, sender, msg):
     nonce = datetime.datetime.now().strftime('%s')
     message = str(nonce) + '.blinktrade.' + str(self.api_key)
     signature = hmac.new(self.api_secret, msg=message, digestmod=hashlib.sha256).hexdigest().upper()
@@ -64,67 +80,6 @@ class BitstampArbitrator(object):
     elif msg['Side'] == '2':
       print datetime.datetime.now(), 'POST https://www.bitstamp.net/api/buy/', str(post_params)
 
-  def on_bitex_balance(self, sender, msg):
-    if str(self.bitex_broker['BrokerID']) in msg:
-      if 'USD' in msg[str(self.bitex_broker['BrokerID'])]:
-        self.usd_balance = msg[str(self.bitex_broker['BrokerID'])]['USD']
-      if 'BTC' in msg[str(self.bitex_broker['BrokerID'])]:
-        self.btc_balance = msg[str(self.bitex_broker['BrokerID'])]['BTC']
-
-  def on_bitex_connected(self, sender, msg):
-    print 'connected to blinktrade'
-    self.bitex_broker = msg['Broker']
-    self.bitex_profile = msg['Profile']
-
-    self.pusher.connection.bind('pusher:connection_established', self.on_bitstamp_connect_handler)
-    self.pusher.connect()
-    self.ws.send(json.dumps({ 'MsgType':'F'}))  # Cancel all open orders for this user
-    self.ws.requestBalances()
-
-
-  def on_send_buy_new_order(self,sender, msg):
-    print datetime.datetime.now(), msg
-    self.ws.sendMsg(msg)
-
-  def on_send_sell_new_order(self,sender, msg):
-    print datetime.datetime.now(), msg
-    self.ws.sendMsg(msg)
-
-  def on_send_cancel_order(self,sender, msg):
-    print datetime.datetime.now(), msg
-    self.ws.sendMsg(msg)
-
-  def on_bitstamp_connect_handler(self, data):
-    print 'connected to bitstamp'
-    channel = self.pusher.subscribe('order_book')
-    channel.bind('data', self.on_bitstamp_order_book_handler )
-
-  def on_bitstamp_order_book_handler(self, data):
-    data = json.loads(data)
-    bid_list = [  [  int(float(usd)*1e8 * (1. - self.bid_fee) ), int(float(btc) * 1e8) ]  for usd,btc in data['bids'] ]
-    ask_list = [  [  int(float(usd)*1e8 * (1. + self.ask_fee) ), int(float(btc) * 1e8) ]  for usd,btc in data['asks'] ]
-    bid_list = get_funded_entries(bid_list, self.usd_balance, True)
-    ask_list = get_funded_entries(ask_list, self.btc_balance, False)
-    self.order_book_bid_processor.process_order_list(bid_list)
-    self.order_book_ask_processor.process_order_list(ask_list)
-
-  def run(self):
-    self.ws.signal_logged.connect(self.on_bitex_connected)
-    self.ws.signal_balance.connect(self.on_bitex_balance )
-    self.ws.signal_execution_report.connect(self.on_bitex_execution_report)
-
-    self.ws.connect()
-    self.ws.login(self.bitex_username, self.bitex_password )
-
-
-    try:
-      self.ws.run_forever()
-    except Exception, e:
-      self.ws.send(json.dumps({'MsgType':'F'}))  # Cancel all open orders for this user
-      self.ws.close()
-      pass
-
-
 
 def main():
   import getpass
@@ -137,14 +92,22 @@ def main():
   sell_fee =  float(raw_input('sell fee [0 - 100]: ')) / 100
 
 
-  arbitrator = BitstampArbitrator(username,
-                                  password,
-                                  websocket_url,
-                                  buy_fee,
-                                  sell_fee,
-                                  'XXXX',
-                                  'YYYY')
-  arbitrator.run()
+  bitstamp_blinktrade_arbitrator = BitStampClient(username,password,websocket_url,buy_fee,sell_fee,'XXXX','YYYY')
+  bitstamp_blinktrade_arbitrator.connect()
+
+  while True:
+    try:
+      bitstamp_blinktrade_arbitrator.keep_alive()
+      sleep(5)
+
+    except KeyboardInterrupt:
+      bitstamp_blinktrade_arbitrator.disconnect()
+      break
+    except Exception,e:
+      print  e
+      break
+
+  bitstamp_blinktrade_arbitrator.disconnect()
 
 main()
 
--- tools/arbitrage/itbit.py
+++ tools/arbitrage/itbit.py
@@ -9,16 +9,16 @@ import datetime
 import hmac
 import hashlib
 
-BITFINEX_API_KEY = 'XXXX'
-BITFINEX_API_SECRET = 'YYYY'
+ITBIT_API_KEY = 'XXXX'
+ITBIT_API_SECRET = 'YYYY'
 
-def send_order_to_bitfinex(sender, order):
+def send_order(sender, order):
   nonce = datetime.datetime.now().strftime('%s')
-  message = 'sendorder' + str(BITFINEX_API_KEY) + str(nonce)
-  signature = hmac.new(BITFINEX_API_SECRET, msg=message, digestmod=hashlib.sha256).hexdigest().upper()
+  message = 'sendorder' + str(ITBIT_API_KEY) + str(nonce)
+  signature = hmac.new(ITBIT_API_SECRET, msg=message, digestmod=hashlib.sha256).hexdigest().upper()
 
   post_params = {
-    'key': BITFINEX_API_KEY,
+    'key': ITBIT_API_KEY,
     'sign': signature,
     'pair': 'btc_brl',
     'volume': float(order['OrderQty']/1.e8),
@@ -30,11 +30,11 @@ def send_order_to_bitfinex(sender, order):
   elif order['Side'] == '2':
     post_params['type'] = 'sell'
 
-  print datetime.datetime.now(), 'POST https://api.bitfinex.com/v1/tapi/' + message, str(post_params)
+  print datetime.datetime.now(), 'POST https://api.hitbtc.com/v1/tapi/' + message, str(post_params)
 
 def main():
   import getpass
-  print "BlinkTrade <-> BitFinex arbitrator"
+  print "BlinkTrade <-> ItBit arbitrator"
   websocket_url = raw_input('BlinkTrade Websocket api server: ')
   username = raw_input('Username: ')
   password = getpass.getpass()
@@ -45,7 +45,7 @@ def main():
 
   arbitrator.connect_to_blinktrade()
 
-  arbitrator.signal_order.connect(send_order_to_bitfinex)
+  arbitrator.signal_order.connect(send_order)
 
   while True:
     try:
@@ -53,7 +53,7 @@ def main():
       arbitrator.send_testRequest()
 
       try:
-        raw_data = urllib2.urlopen('https://api.bitfinex.com/v1/book/BTCUSD').read()
+        raw_data = urllib2.urlopen('https://www.itbit.com/api/v2/markets/XBTUSD/orders').read()
       except Exception:
         print 'ERROR RETRIEVING ORDER BOOK'
         continue
@@ -66,8 +66,8 @@ def main():
         pass
 
       if bids_asks:
-        bid_list = [ [  int(float(o['price']) * 1e8 * (1. + bid_fee) ) , int( float(o['amount']) * 1e8) ] for o in bids_asks['bids'] ]
-        ask_list = [ [  int(float(o['price']) * 1e8 * (1. + ask_fee) ) , int( float(o['amount']) * 1e8) ] for o in bids_asks['asks'] ]
+        bid_list = [ [  int(float(o[0]) * 1e8 * (1. + bid_fee) ) , int( float(o[1]) * 1e8) ] for o in bids_asks['bids'] ]
+        ask_list = [ [  int(float(o[0]) * 1e8 * (1. + ask_fee) ) , int( float(o[1]) * 1e8) ] for o in bids_asks['asks'] ]
         arbitrator.process_ask_list(ask_list)
         arbitrator.process_bid_list(bid_list)
     except urllib2.URLError as e:
