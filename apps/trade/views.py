a6fff322ad3c30b1076a24118fb4a523d1e64251
Rodrigo <rodrigo@blinktrade.com>
2015-01-28 02:38:12 -0500

    limit filter comparasion operators

--- apps/trade/views.py
+++ apps/trade/views.py
@@ -319,7 +319,7 @@ def processCancelOrderRequest(session, msg):
   else:
     # user cancelling all the orders he sent.
     orders = Order.get_list(TradeApplication.instance().db_session,
-                            [ "user_id eq " + str(session.user.id), "leaves_qty gt 0"])
+                            [ "user_id eq " + str(session.user.id), "leaves_qty ne 0"])
     for order in orders:
       order_list.append(order)
 
